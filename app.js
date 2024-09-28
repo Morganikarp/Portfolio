42c5282d14de2aae630d55cf4b954f9772cd9a21
Morganikarp <MorganBowen10@hotmail.com>
2024-09-28 12:21:46 +0100

    initial

---
+++ app.js
@@ -0,0 +1,15 @@
+function imgHighlight(img){
+    img.style.opacity = "0.8";
+}
+
+function imgBase(img){
+    img.style.opacity = "1";
+}
+
+function subjectHighlight(subject){
+    subject.style.background = "#bc453c";
+}
+
+function subjectBase(subject){
+    subject.style.background = "#b02727";
+}
